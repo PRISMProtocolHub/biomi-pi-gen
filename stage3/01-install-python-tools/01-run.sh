4f99773a83f048c195c019534bd305383fcb1973
Hugo Jacquet <hugo.jacquet@kheops.ch>
2025-05-07 17:57:09 +0200

    feat(stages): Customize the stages to include the biomi specific packages + dev dependencies (pdm, venv)

---
+++ stage3/01-install-python-tools/01-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+echo "Install pdm for ${FIRST_USER_NAME}..."
+on_chroot << EOF
+    su - ${FIRST_USER_NAME} -c "curl -sSLO https://pdm-project.org/install-pdm.py"
+    su - ${FIRST_USER_NAME} -c "curl -sSL https://pdm-project.org/install-pdm.py.sha256 | shasum -a 256 -c -"
+    su - ${FIRST_USER_NAME} -c "python3 install-pdm.py"
+EOF
