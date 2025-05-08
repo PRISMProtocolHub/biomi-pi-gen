8dca49b93fd44e3083db178b77d71d439b9f8a23
Hugo Jacquet <hugo.jacquet@kheops.ch>
2025-05-08 11:01:16 +0200

    feat(biomi-packages): Reorganize and add missing biomi required packages

--- stage3/01-install-python-tools/01-run.sh
+++ stage3/01-install-python-tools/01-run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -e
 
+echo "Install python building tools..."
+on_chroot << EOF
+    su - ${FIRST_USER_NAME} -c "pip install --upgrade pip setuptools wheel"
+EOF
+
 echo "Install pdm for ${FIRST_USER_NAME}..."
 on_chroot << EOF
     su - ${FIRST_USER_NAME} -c "curl -sSLO https://pdm-project.org/install-pdm.py"
     su - ${FIRST_USER_NAME} -c "curl -sSL https://pdm-project.org/install-pdm.py.sha256 | shasum -a 256 -c -"
     su - ${FIRST_USER_NAME} -c "python3 install-pdm.py"
+
     su - ${FIRST_USER_NAME} -c "export PATH=/home/${FIRST_USER_NAME}/.local/bin:$PATH"
 EOF
