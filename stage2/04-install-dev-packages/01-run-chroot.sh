bf33c40b6913dd658c764d014c5176a50911e3c4
Hugo Jacquet <hugo.jacquet@kheops.ch>
2025-06-04 10:37:25 +0200

    feat(pipx): Fix pipx installation

--- stage2/04-install-dev-packages/01-run-chroot.sh
+++ stage2/04-install-dev-packages/01-run-chroot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-python3 -m pip install --user pipx
+pip install --user pipx
 
 sudo -u pi bash -c 'curl -sSL https://pdm-project.org/install-pdm.py | python3 -'
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> /home/pi/.bashrc
