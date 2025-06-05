f28c3e153132d218860a43067b5440b9abeb309c
Hugo Jacquet <hugo.jacquet@kheops.ch>
2025-06-05 17:44:25 +0200

    feat(docker+serial): Fix docker image push and serial communication with atmel

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -2,3 +2,8 @@
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+
+# Use raspi-config to enable the serial shell
+on_chroot << EOF
+raspi-config nonint do_serial 0
+EOF
