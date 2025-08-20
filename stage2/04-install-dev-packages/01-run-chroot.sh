0cd9356d67c4f90fe005c7dc495b1d5c202a84bc
Hugo Jacquet <hugo.jacquet@kheops.ch>
2025-08-20 11:32:50 +0200

    ci(pdm): Fix pdm installation using pipx

--- stage2/04-install-dev-packages/01-run-chroot.sh
+++ stage2/04-install-dev-packages/01-run-chroot.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -e
 
-pip install --user pipx
-python3 -m venv myproject
-
-sudo -u pi bash -c 'curl -sSL https://pdm-project.org/install-pdm.py | python3 -'
-sudo -u pi bash -c 'echo "export PATH=\"\$HOME/.local/bin:\$PATH\"" >> ~/.bashrc'
+# Install pdm using PIPX as official installation doesn't work in that context
+pipx install pdm
