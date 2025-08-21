9dd491c6e27153ce79ef2292c701c716bd3ae9f1
Hugo Jacquet <hugo.jacquet@kheops.ch>
2025-08-21 16:37:26 +0200

    ci(camera-config): Add a debug trace

--- stage2/05-install-dev-packages/00-run.sh
+++ stage2/05-install-dev-packages/00-run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 
+echo "=== install camera custom config imx708_wide_biomi_custom.json ==="
 install -v -m 644 files/imx708_wide_biomi_custom.json "${ROOTFS_DIR}/usr/share/libcamera/ipa/rpi/vc4/"
