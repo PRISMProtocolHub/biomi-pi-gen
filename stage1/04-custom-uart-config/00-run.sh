0bacf1473bbd9bb60382de145e037cdd3b6e6a46
Hugo Jacquet <hugo.jacquet@kheops.ch>
2025-08-25 11:49:54 +0200

    ci(serial): Fix the serial port activation by using only the config.txt

--- stage1/04-custom-uart-config/00-run.sh
+++ stage1/04-custom-uart-config/00-run.sh
@@ -4,11 +4,5 @@ echo "=== UART Custom Configuration  ==="
 echo "enable_uart=1" >> "${ROOTFS_DIR}/boot/firmware/config.txt"
 
 sed -i 's/console=serial0,[0-9]\+\s*//g' "${ROOTFS_DIR}/boot/firmware/cmdline.txt"
-sed -i 's/console=ttyAMA0,[0-9]\+\s*//g' "${ROOTFS_DIR}/boot/firmware/cmdline.txt"
-
-on_chroot << 'EOF'
-systemctl disable serial-getty@ttyAMA0.service 2>/dev/null || true
-usermod -a -G dialout pi
-EOF
 
 echo "=== UART customized ==="
